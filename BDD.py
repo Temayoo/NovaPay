9966572bc3ee51ed54ed37a729e6d58cf50c73c8
Lucky31001 <kilian.dumaire@gmail.com>
2025-01-13 16:24:06 +0100

    prettier

--- BDD.py
+++ BDD.py
@@ -6,18 +6,22 @@ sqlite_url = f"sqlite:///{sqlite_file_name}"
 connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, connect_args=connect_args)
 
+
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str = Field(index=True)
     email: str = Field(index=True)
     password: str
 
+
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
+
 
 def get_session():
     with Session(engine) as session:
         yield session
+
 
 # Crée les tables si elles n'existent pas déjà
 create_db_and_tables()

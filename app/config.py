7bc98e9173c3313742dbf0ddbba5b06e054574c6
Lucky31001 <kilian.dumaire@gmail.com>
2025-01-17 12:52:43 +0100

    CORS

--- app/config.py
+++ app/config.py
@@ -1,3 +1,7 @@
 SECRET_KEY = "96f64b557bc4963f4fe85d804d7a7796d6fe26d8fbe98496d7f0e9271bba38d7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ORIGIN = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
